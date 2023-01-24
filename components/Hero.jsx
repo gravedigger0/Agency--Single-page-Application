f4fbf5c0c24057d9f1a3bc862dbb7e75f352e6a8
rafat <hoqrafat@gmail.com>
2023-01-24 16:31:45 +0600

    added section in Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,8 +7,13 @@ const Hero = () => {
         <div className="h-[88vh] w-full relative bg-">
             <Image src="/Vector.svg" fill objectFit="cover" className="" />
             <div className="absolute top-0 left-0 w-full h-full">
-                <div className="mt-[8rem] mx-[10rem] mb-[5rem]">
+                <div className="mt-[8rem] mx-[10rem] mb-[5rem] flex">
+                    <section className="text-black w-[40%] h-full">
                     <h1 className="text-2xl font-bold tracking-tight text-accent">Digital Marketing</h1>
+                    </section>
+                    <section className="text-black w-[60%] h-full border">
+                        Image
+                    </section>
                 </div>
             </div>
         </div>
